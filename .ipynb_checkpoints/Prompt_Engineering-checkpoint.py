a4c37151a5f5e70ec9aad2381f1e2d6e1f51050e
guillermoih <guillermo.iglesias@upm.es>
2024-03-20 12:35:31 +0000

    Code refactored and added sentence similarity

--- .ipynb_checkpoints/Prompt_Engineering-checkpoint.py
+++ .ipynb_checkpoints/Prompt_Engineering-checkpoint.py
@@ -1,46 +1,95 @@
-import os
-os.system("pip install -U sentence-transformers")
+# +
+#import os
+#os.system("pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html")
+# -
+
+# # Imports
 
 # +
-import numpy as np
+import json, requests
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 
 from diffusers import StableDiffusionXLPipeline
 from transformers import BlipProcessor, BlipForConditionalGeneration
-from sentence_transformers import SentenceTransformer, util
+# -
+
+# # Models load
+
+# ## Stable Diffusion XL
+
+print("#"*5, "Loading Stable Diffusion XL", "#"*5)
 
 # +
-pipeline_text2image = StableDiffusionXLPipeline.from_pretrained(
+STABLE_DIFF_PIPELINE = StableDiffusionXLPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
 )
 
-pipeline_text2image.to("cuda")
+STABLE_DIFF_PIPELINE.to("cuda")
 # -
 
-prompt = "Hello Kitty in the space riding a bike"
-images = pipeline_text2image(prompt=prompt, num_images_per_prompt=1,
+# ## BLIP
+
+print("#"*5, "Loading BLIP", "#"*5)
+
+BLIP_PROCCESOR = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+BLIP_MODEL = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+
+# # Caption generation using Blip
+
+def generate_caption(img):
+    inputs = BLIP_PROCCESOR(img, return_tensors="pt")
+
+    out = BLIP_MODEL.generate(**inputs)
+    return BLIP_PROCCESOR.decode(out[0], skip_special_tokens=True)
+
+
+# ## Sentence similarity with DistilBERT
+
+# +
+API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/msmarco-distilbert-base-tas-b"
+api_token = "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"
+headers = {"Authorization": f"Bearer {api_token}"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+# -
+
+def sentence_similarity(input_sentence, sentences):
+    data = query(
+        {
+            "inputs": {
+                "source_sentence": input_sentence,
+                "sentences": sentences
+            }
+        })
+    
+    return data
+
+
+prompt = "Hello Kitty in the space riding a bicycle"
+images = STABLE_DIFF_PIPELINE(prompt=prompt, num_images_per_prompt=5,
                              output_type='pil').images
 
+# +
+captions = []
 for i, img in enumerate(images):
     img.save("test_" + str(i) + ".jpg")
+    
+    print("Processing mage", i)
+    caption = generate_caption(img)
+    print("Generated caption:", caption)
+    captions.append(caption)
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+similarities = sentence_similarity(prompt, captions)
+# -
 
-# +
-inputs = processor(images[0], return_tensors="pt")
-
-out = model.generate(**inputs)
-caption = processor.decode(out[0], skip_special_tokens=True)
-print(caption)
-
-# +
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
-embedding_1= model.encode(prompt, convert_to_tensor=True)
-embedding_2 = model.encode(caption, convert_to_tensor=True)
-
-similarity = util.pytorch_cos_sim(embedding_1, embedding_2)
-
-print(similarity)
+for caption, similarity in zip(captions, similarities):
+    print("Similarity for:", caption)
+    print(similarity)
+    print("\n", "-"*10)
